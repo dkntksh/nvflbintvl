e7bad24ebb90d54b1cf990f80a2f0ed9fd619094
dnkntksh <mr.kaz71@gmail.com>
2019-06-17 22:47:19 +0900

    ファイル選択ダイアログの実装 転送ボタンクリック時に転送ボタンを非活性にする

--- MoveFileByInternal/MainPage.xaml.cs
+++ MoveFileByInternal/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -37,21 +38,77 @@ namespace MoveFileByInternal
 
         }
 
+        /*
+         * FROM選択ボタンクリック時
+         * 
+         */
         private async void From_select_button_Click(object sender, RoutedEventArgs e)
         {
-            var filePicker = new Windows.Storage.Pickers.FileOpenPicker();
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
 
             // 選択可能な拡張子を追加
-            filePicker.FileTypeFilter.Add("*");
+            folderPicker.FileTypeFilter.Add("*");
 
-            Windows.Storage.StorageFile file = await filePicker.PickSingleFileAsync();
+            Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
 
-            if (file == null)
+            if (folder == null)
             {
                 return;
             }
 
-            from_select_textbox.Text = file.Path.ToString();
+            from_select_textbox.Text = folder.Path.ToString();
+        }
+
+        /*
+         * TO選択ボタンクリック時
+         * 
+         */
+        private async void To_select_button_Click(object sender, RoutedEventArgs e)
+        {
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+
+            // 選択可能な拡張子を追加
+            folderPicker.FileTypeFilter.Add("*");
+
+            Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+
+            if (folder == null)
+            {
+                return;
+            }
+
+            to_select_textbox.Text = folder.Path.ToString();
+        }
+
+        /*
+         * 転送ボタンクリック時
+         * 
+         */
+        private void Start_button_click(object sender, RoutedEventArgs e)
+        {
+            // 転送ボタンを非活性
+            start_button.IsEnabled = true;
+            // validate
+
+            // エラーの場合、エラーメッセージを表示し、転送ボタンを活性化する
+
+            // fromディレクトリを回して、フォルダorファイルを１つ１つ取得
+
+            // 対象のフォルダorファイルを転送先にファイルを転送する
+
+            // 転送したファイルをログに出力
+
+            // 指定の秒数スリープする
+            SleepAsync(3);
+
+            // 全ての終了後、転送ボタンを活性化する
+            start_button.IsEnabled = false;
+
+        }
+
+        private async void SleepAsync(int minites)
+        {
+            await Task.Delay(minites * 1000);
         }
     }
 
